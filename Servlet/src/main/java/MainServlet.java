2ac99bc2bf11bea8e487a04752bc5f38407171bf
mnnogo <akireev86348634@gmail.com>
2024-03-31 15:00:49 +0600

    finished redirect with servlets

---
+++ Servlet/src/main/java/MainServlet.java
@@ -0,0 +1,40 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/nav"})
+@MultipartConfig
+public class MainServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        int pageNumber = Integer.parseInt(request.getParameter("page"));
+
+        switchPageTo(pageNumber, request, response);
+    }
+
+    private void switchPageTo(int pageNumber, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        String path = "";
+        switch (pageNumber)
+        {
+            case 1 -> path = "page1.html";
+            case 2 -> path = "page2.html";
+            case 3 -> path = "page3.html";
+            case 4 -> path = "page4.html";
+            default -> response.sendError(HttpServletResponse.SC_NOT_FOUND, "Страница не найдена");
+        }
+
+        if (path.isEmpty())
+            return;
+
+        RequestDispatcher view = request.getRequestDispatcher(path);
+        view.forward(request, response);
+    }
+}
